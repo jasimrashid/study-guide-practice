ab0f678d7fd9ee887a085d11e9b42a0cd6f946ee
Jasim Rashid <jasim.rashid@gmail.com>
2020-06-19 14:11:53 +0530

    connect to API

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # study-guide-practice
+
+## Installation
+
+Download repo and navigate there from CL:
+```sh
+
+```
+
+## Setup
+```sh
+pipenv python 3.7
+pipenv shell
+```
+
+Install packages
+- beautiful soup
+- requests and parser
+
+```sh
+pipenv install beautifulsoup4
+pipenv install requests
+
+
+```
+
+## Usage
+
--- web_app/services/board_games.py
+++ web_app/services/board_games.py
@@ -21,31 +21,44 @@ game_id = 13
 url = 'https://www.boardgamegeek.com/xmlapi/boardgame/' + str(game_id)
 result = requests.get(url)
 soup = bs4.BeautifulSoup(result.text, features='lxml')
-print(soup.find('name').text)
+print('Name: ', soup.find('name').text,'\n')
+print('Min players: ', soup.find('minplayers').text, '\n')
+print('Max players: ', soup.find('maxplayers').text, '\n')
+print('Playing time: ', soup.find('playingtime').text, '\n')
+print('Description: ', soup.find('description').text, '\n')
+print('Board game mechanics:')
+boardgamemechanic = soup.find_all('boardgamemechanic')
+for i in boardgamemechanic: print(i.text)
+
+
+# for multiple
+# temp = soup.find_all('name')
+# for i in temp: print(i)
+
+print()
+# breakpoint()
 
 
 if __name__ == "__main__":
-    user = api.get_user("realDonaldTrump")
-    print("TWITTER USER:", type(user))
+    # user = api.get_user("realDonaldTrump")
+    # print("TWITTER USER:", type(user))
 
-    print(user.id)
-    print(user.screen_name)
-    print(user.name)
-
-
-    tweets = api.user_timeline("realDonaldTrump", tweet_mode = "extended")
-    print("TWEETS", type(tweets))
-    print(type(tweets[0]))
+    # print(user.id)
+    # print(user.screen_name)
+    # print(user.name)
 
 
-    tweet = tweets[0]
-    print(tweet.id)
-    print(tweet.full_text)
-    breakpoint()
+    # tweets = api.user_timeline("realDonaldTrump", tweet_mode = "extended")
+    # print("TWEETS", type(tweets))
+    # print(type(tweets[0]))
 
 
-import requests
-import bs4
+    # tweet = tweets[0]
+    # print(tweet.id)
+    # print(tweet.full_text)
+    # breakpoint()
+    print('OK')
+
 
 
 
