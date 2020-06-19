523cfef15e9642f1ebc88cf0f20c7458f489bec7
Jasim Rashid <jasim.rashid@gmail.com>
2020-06-19 17:03:27 +0530

    updated home routes. new DB model. git push origin master xit exit

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,25 +1,25 @@
 from flask import Flask
-# from web_app.models import db, migrate
+from models.models import db, migrate
 from web_app.routes.home_routes import home_routes
 # from web_app.routes.book_routes import book_routes
 # from web_app.routes.twit_routes import twit_routes
 # from web_app.routes.twitter_routes import twitter_routes
 # from web_app.routes.stats_routes import stats_routes
 
-# DATABASE_URL = "sqlite:///twitoff_development.db" # using relative filepath
+DATABASE_URL = "sqlite:///boardgame_development.db" # using relative filepath
 # SECRET_KEY = "todo customize this secret value via env var"
-#DATABASE_URI = "sqlite:////Users/Username/Desktop/your-repo-name/web_app_99.db" # using absolute filepath on Mac (recommended)
-#DATABASE_URI = "sqlite:///C:\\Users\\Username\\Desktop\\your-repo-name\\web_app_99.db" # using absolute filepath on Windows (recommended) h/t: https://stackoverflow.com/a/19262231/670433
+# DATABASE_URI = "sqlite:////Users/Username/Desktop/your-repo-name/web_app_99.db" # using absolute filepath on Mac (recommended)
+# DATABASE_URI = "sqlite:///C:\\Users\\Username\\Desktop\\your-repo-name\\web_app_99.db" # using absolute filepath on Windows (recommended) h/t: https://stackoverflow.com/a/19262231/670433
 
 
 def create_app():
     app = Flask(__name__)
 
     # app.config["SECRET_KEY"] = SECRET_KEY
-    # app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
-    # app.config["SQLALCHEMY_TRACK_MODIFICATION"] = DATABASE_URL
-    # db.init_app(app)
-    # migrate.init_app(app, db)
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+    app.config["SQLALCHEMY_TRACK_MODIFICATION"] = DATABASE_URL
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     app.register_blueprint(home_routes)
     # app.register_blueprint(book_routes)
--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,23 +1,35 @@
 # web_app/routes/home_routes.py
-from flask import Blueprint, render_template
+from flask import Blueprint, jsonify, request, render_template #, flash, redirect
+from models.models import BoardGame, db, parse_records
 # from web_app.models import User
 home_routes = Blueprint("home_routes", __name__)
 
 @home_routes.route("/")
 def index():
-    # print('Hello world!')
-    # screen_names = User.query.with_entities(User.screen_name).distinct()
-    # for screen_name in screen_names:
-        # print(screen_name[0])
-    # breakpoint()
-
-    # FETCH USERS FROM DATABASE
-    # breakpoint()
-
-
-
     return 'Hello World!'
     # return render_template("prediction_form.html", screen_names=screen_names)
+
+    # PRINT RECORDS FROM DATABASE
+
+@home_routes.route("/add/<game_id>")
+def fetch_and_add(game_id=None):
+
+    # FETCH DATA FROM BGG API
+
+    # STORE GAME DATA INTO DB
+    # parse into objects > store into db
+    db_game = BoardGame(game_id = game_id)
+    db_game.name = 'game1'
+    db_game.minplayers = 1
+    db_game.maxplayers = 2
+    db_game.playingtime = 10
+    db_game.description = 'game1'
+
+    breakpoint()
+    
+    db.session.add(db_game)
+    db.session.commit()
+    
 
 @home_routes.route("/hello")
 def hello():
